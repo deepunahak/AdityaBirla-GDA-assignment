7133ce2e5a3389e66f873210cbd8d7686513d5c5
deepunahak <deepunahak@gmail.com>
2022-01-30 23:42:23 +0530

    flask unit test added

--- flask_app/test.py
+++ flask_app/test.py
@@ -17,6 +17,24 @@ class TestFlaskApis(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    def test_bank_leader_board(self):
+        url = '/bank_leader_board'
+
+        load_data(config.path)
+        response = self.app.get(url, headers=self.headers)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_statistics(self):
+        url = '/ifsc_search?ifsc_code=ABHY0065001'
+        load_data(config.path)
+        response = self.app.get(url, headers=self.headers)
+        if response.status_code == 200:
+            url_statistics = '/statistics'
+            resp = self.app.get(url_statistics, headers=self.headers)
+
+            self.assertEqual(resp.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
